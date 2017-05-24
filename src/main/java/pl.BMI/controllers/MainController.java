0e6ec430c9988f27ad928d0228ca1514dfb5dba3
Mateusz Marczuk <mateuszmarczuk95@gmail.com>
2017-05-24 11:30:32 +0200

    Add TopMenuButtons and method onAction.

--- src/main/java/pl.BMI/controllers/MainController.java
+++ src/main/java/pl.BMI/controllers/MainController.java
@@ -1,12 +1,15 @@
 package pl.BMI.controllers;
 
 
+import javafx.fxml.FXML;
 
 /**
  * Created by Mateusz on 23.05.2017.
  */
 public class MainController {
 
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
 
 
 }
---
+++ src/main/java/pl.BMI/controllers/TopMenuButtonsController.java
@@ -0,0 +1,23 @@
+package pl.BMI.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Mateusz on 24.05.2017.
+ */
+public class TopMenuButtonsController {
+
+    @FXML
+    public void openCar() {
+    }
+
+    @FXML
+    public void openRentCar() {
+    }
+
+
+    @FXML
+    public void openStatistic() {
+    }
+}
